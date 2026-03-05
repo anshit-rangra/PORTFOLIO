946a019e841530ff51bddc2b4487811470c263b5
anshit-rangra <anshit22rangra@gmail.com>
2026-03-05 14:12:30 +0530

    adding my url

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = "https://anshitrangra.dev";
+  const baseUrl = "https://anshit-portfolio-pi.vercel.app";
 
   return {
     rules: [
